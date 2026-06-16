46d319a20303afe60ae2f075e6facbfafb40bb0b
sumiyariaz6 <sumiyariaz6@gmail.com>
2026-06-17 00:48:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,22 @@ uploaded_file = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
 if uploaded_file:
     image = Image.open(uploaded_file)
 
-    st.image(image)
+    st.image(image, caption="Original Image")
 
     if st.button("Detect"):
         results = model(np.array(image))
 
         annotated = results[0].plot()
+        st.image(annotated, caption="Detected Image", channels="BGR")
 
-        st.image(annotated, channels="BGR")
+        # ⭐ OBJECT COUNT ADDED HERE
+        counts = {}
+
+        for box in results[0].boxes:
+            cls_id = int(box.cls[0])
+            class_name = model.names[cls_id]
+
+            counts[class_name] = counts.get(class_name, 0) + 1
+
+        st.subheader("Object Count")
+        st.write(counts)
